f3647206323cbe9bf4ae8569670a7e672d637756
Stepan Karpov <stepan-karpov-2025@mail.ru>
2025-11-26 13:21:04 +0300

    rework schema

--- app/core/schemas/operator.py
+++ app/core/schemas/operator.py
@@ -1,23 +1,35 @@
+from typing import Dict
+
 from pydantic import BaseModel, ConfigDict
 
 
 class OperatorBase(BaseModel):
     model_config = ConfigDict(
         from_attributes=True,
-        arbitrary_types_allowed=True
     )
 
 
 class OperatorCreate(OperatorBase):
-    is_active: bool
-    limit: int
+    name: str
+    is_active: bool = True
+    max_load_limit: int = 10
+    competencies: Dict[str, float] = {}
+
 
 
 class OperatorUpdate(OperatorBase):
-    pass
+    name: str | None = None
+    is_active: bool | None = None
+    max_load_limit: int | None = None
+    competencies: Dict[str, float] | None = None
 
 
 class OperatorResponse(OperatorBase):
     id: int
+    name: str
     is_active: bool
-    limit: int
+    max_load_limit: int
+    current_load: int
+
+class OperatorContactsResponse(OperatorResponse):
+    contacts: list = []
